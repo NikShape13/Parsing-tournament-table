bd1f11b61a46c8fa1f0b184992688b7ced7f5817
mephistaust <162083687+mephistaust@users.noreply.github.com>
2024-04-11 16:31:33 +0300

    Add files via upload

---
+++ Celestia/main.py
@@ -0,0 +1,54 @@
+import requests
+import json
+from fake_useragent import UserAgent as US
+from bs4 import BeautifulSoup as BS
+
+
+user_agent = US().random
+
+
+headers = {
+    'authority': '7nkwv3z5t1.execute-api.us-east-1.amazonaws.com',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'ru,en;q=0.9',
+    'origin': 'https://analytics.smartstake.io',
+    'referer': 'https://analytics.smartstake.io/',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "YaBrowser";v="24.1", "Yowser";v="2.5"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': user_agent,
+}
+
+params = {
+    'pageSize': '500',
+    'page': '1',
+    'type': 'address',
+    'key': '2mwTEDr9zXJH323M',
+    'token': '1712840836',
+    'app': 'TIA',
+}
+
+
+with open('adress.txt') as f:
+    adress_list = f.read().splitlines()
+
+
+for i in range(1, 1033):
+    r = requests.get(
+        'https://7nkwv3z5t1.execute-api.us-east-1.amazonaws.com/prod/richlist', params=params, headers=headers)
+    r.encoding = 'utf-8'
+
+    dicts = r.json()['data']
+
+    for i in dicts:
+        if i['address'] in adress_list:
+            with open('fin.json', 'a')as f:
+                json.dump(i, f, indent=4)
+
+            if i['address'] == adress_list[-1]:
+                print('Done')
+
+    params['page'] = str(int(params['page']) + 1)
